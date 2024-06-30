c32ecd7d779b55514c990f7a91dd0e1d1281e94a
Viktoriia Nykyforova <113258083+ViktoriiaKN@users.noreply.github.com>
2024-06-30 15:35:57 +0200

    task 3

--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,8 +1,11 @@
 import styled from './Statistics.module.css'
+import { IconContext } from "react-icons";
+
+
 export const StatisticsItem = ({ stat, icon }) => {
   return (
     <li className={styled.item}>
-      {icon}
+      <IconContext.Provider value={{color: 'tomato', size: 28}}> {icon} </IconContext.Provider>
       <span className={styled.counter}>{stat.total}</span>
       <p className={styled.text}>{stat.title}</p>
     </li>
