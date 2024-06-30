1e14bd2b1f307b7cb1102612f7fe9fc2ca076cdb
Oleh Ocheretko <117608037+Ocheretko1992@users.noreply.github.com>
2024-06-30 16:09:26 +0300

    les 2

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import {
   ForbesList,
 } from 'components';
 import artcle from './data/article.json';
+import statistics  from './data/stats.json'
 
 export const App = () => {
   return (
@@ -25,7 +26,7 @@ export const App = () => {
         />
 
         <Heading title="Task 2 Statistics" top bottom />
-        <Statistics title="Main Statistics" />
+        <Statistics title="Main Statistics" stats={statistics} />
 
         <Heading title="Task 3 Forbes list" top bottom />
         <ForbesList />
--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,3 +1,10 @@
-export const StatisticsItem = () => {
-  return <h3>StatisticsItem</h3>;
+import styled from './Statistics.module.css'
+export const StatisticsItem = ({ stat, icon }) => {
+  return (
+    <li className={styled.item}>
+      {icon}
+      <span className={styled.counter}>{stat.total}</span>
+      <p className={styled.text}>{stat.title}</p>
+    </li>
+  );
 };
